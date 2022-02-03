4c3823929e1eb9ee43985adc2cbb8d0461bcb213
Richard Guan <rguan72@gmail.com>
2022-02-03 14:34:39 -0500

    Make virtual environment and add pyenchant dependency

---
+++ possible_words.py
@@ -0,0 +1,7 @@
+# given:
+#    letters that must be used
+#    letters that cannot be used
+#    letters in specific positions
+
+# generate all possible 5 letter words
+# this can be re-used for finding words that contain letters that cut the list of possible words in half
